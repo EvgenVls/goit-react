05ee3dc41d5ffd52ccec8ace62b8dcb813e76048
Yevgen Vlasenko <evgenvl@ukr.net>
2024-04-14 20:55:01 +0300

    all

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+// import { useState } from 'react'
+import FeedbackForm from "../FeedbackForm/FeedbackForm";
+import "./App.css";
+
+function App() {
+  return <FeedbackForm />;
+}
+
+export default App;
