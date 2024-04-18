807b0b12acad2ea1b4688810ae2093aa6369ae1e
EvgenVls <145988014+EvgenVls@users.noreply.github.com>
2024-04-18 18:48:16 +0300

    featch

--- src/articles-api.js
+++ src/articles-api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://hn.algolia.com/api/v1";
 
-export const featchArticiesWithTotic = async (topic) => {
-  const response = axios.get(`/search?query=${topic}`);
+export const featchArticlesWithTotic = async (topic) => {
+  const response = await axios.get(`/search?query=${topic}`);
   return response.data.hits;
 };
