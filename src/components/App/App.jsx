a255f3205df15725063abe1e65d10140629dc78a
EvgenVls <145988014+EvgenVls@users.noreply.github.com>
2024-04-19 17:18:45 +0300

    mem

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,38 +1,25 @@
 import { useState } from "react";
-import ArticleList from "../ArticleList/ArticleList";
-import { featchArticlesWithTotic } from "../../articles-api";
-import SearchForm from "../SearchForm/SerrchForm";
+
 import "./App.css";
 
 function App() {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [planets, setPlanets] = useState(["Earth", "Mars", "Jupiter", "Venus"]);
+  const [query, setQuery] = useState("");
+  const [click, setClick] = useState(0);
 
-  const handleSearch = async (topic) => {
-    try {
-      setArticles([]);
-      setError(false);
-      setLoading(true);
-      const data = await featchArticlesWithTotic(topic);
-      setArticles(data);
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const filteredPlanets = planets.filter((planet) => planet.includes(query));
 
   return (
-    <div>
-      <SearchForm onSearch={handleSearch} />
-      <h1>Latest articles</h1>
-      {loading && <p>Loading data, please wait...</p>}
-      {error && (
-        <p>Whoops, something went wrong! Please try reloading this page!</p>
-      )}
-      {articles.length > 0 && <ArticleList items={articles} />}
-    </div>
+    <>
+      <button onClick={() => setClick(click + 1)}>
+        Number of clicks: {click}
+      </button>
+      <ul>
+        {filteredPlanets.map((planet) => (
+          <li key={planet}>{planet}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
