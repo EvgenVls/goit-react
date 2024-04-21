5777e37ab91a2f9103a27fe48315af445b012fef
Yevgen Vlasenko <evgenvl@ukr.net>
2024-04-21 09:22:34 +0300

    pagination

--- src/articles-api.js
+++ src/articles-api.js
@@ -2,7 +2,13 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://hn.algolia.com/api/v1";
 
-export const featchArticlesWithTotic = async (topic) => {
-  const response = await axios.get(`/search?query=${topic}`);
+export const featchArticlesWithTotic = async (topic, currentPage) => {
+  const response = await axios.get("/search", {
+    params: {
+      query: topic,
+      hitsPerPage: 10,
+      page: currentPage,
+    },
+  });
   return response.data.hits;
 };
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ArticleList from "../ArticleList/ArticleList";
 import { featchArticlesWithTotic } from "../../articles-api";
 import SearchForm from "../SearchForm/SerrchForm";
@@ -8,32 +8,68 @@ function App() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
 
-  const handleSearch = async (topic) => {
-    try {
-      setArticles([]);
-      setError(false);
-      setLoading(true);
-      const data = await featchArticlesWithTotic(topic);
-      setArticles(data);
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
+  const handleSearch = (newQuery) => {
+    setQuery(newQuery);
+    setPage(1);
+    setArticles([]);
   };
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+    console.log(page);
+  };
+
+  useEffect(() => {
+    if (query === "") {
+      return;
+    }
+    async function getArticles() {
+      try {
+        setError(false);
+        setLoading(true);
+        const data = await featchArticlesWithTotic(query, page);
+        setArticles((prevArticles) => {
+          return [...prevArticles, ...data];
+        });
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getArticles();
+  }, [query, page]);
 
   return (
     <div>
-      <SearchForm onSearch={handleSearch} />
       <h1>Latest articles</h1>
-      {loading && <p>Loading data, please wait...</p>}
+      <SearchForm onSearch={handleSearch} />
+
       {error && (
         <p>Whoops, something went wrong! Please try reloading this page!</p>
       )}
       {articles.length > 0 && <ArticleList items={articles} />}
+      {loading && <p>Loading data, please wait...</p>}
+      {articles.length > 0 && !loading && (
+        <button onClick={handleLoadMore}>Load more</button>
+      )}
     </div>
   );
 }
 
 export default App;
+
+// try {
+//   setArticles([]);
+//   setError(false);
+//   setLoading(true);
+//   const data = await featchArticlesWithTotic(topic);
+//   setArticles(data);
+// } catch (error) {
+//   setError(true);
+// } finally {
+//   setLoading(false);
+// }
