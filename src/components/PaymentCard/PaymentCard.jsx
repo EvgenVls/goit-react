223fb15ef1f3b21321e2eae258079b626afeda7d
Yevgen Vlasenko <evgenvl@ukr.net>
2024-05-02 07:52:02 +0300

    PaymentCard and PaymentList

---
+++ src/components/PaymentCard/PaymentCard.jsx
@@ -0,0 +1,19 @@
+import css from "./PaymentCard.module.css";
+
+export default function PaymentCard({
+  payment: { id, cardOwner, amount, description },
+}) {
+  return (
+    <div className={css.wrapper}>
+      <p>
+        <b>Card owner:</b> {cardOwner}
+      </p>
+      <p>
+        <b>Amount:</b> {amount}
+      </p>
+      <p>
+        <b>Description:</b> {description}
+      </p>
+    </div>
+  );
+}
--- src/components/PaymentsList/PaymentsList.jsx
+++ src/components/PaymentsList/PaymentsList.jsx
@@ -1,1 +1,14 @@
-export default function PaymentsList() {}
+import PaymentCard from "../PaymentCard/PaymentCard";
+import css from "./PaymentsList.module.css";
+
+export default function PaymentsList({ payments }) {
+  return (
+    <ul className={css.list}>
+      {payments.map((payment) => (
+        <li key={payment.id}>
+          <PaymentCard payment={payment} />
+        </li>
+      ))}
+    </ul>
+  );
+}
