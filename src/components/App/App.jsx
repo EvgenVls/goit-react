81e7533becd9c18410aed334ae9c038ddc61919c
Yevgen Vlasenko <evgenvl@ukr.net>
2024-05-09 07:33:27 +0300

    structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,34 +1,13 @@
-import { lazy } from "react";
-import { Route, Routes } from "react-router-dom";
-import Layout from "../Layout/Layout";
-// import css from "./App.module.css";
-
-const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
-const PaymentsPage = lazy(() =>
-  import("../../pages/PaymentsPage/PaymentsPage")
-);
-const PageNotFound = lazy(() =>
-  import("../../pages/PageNotFound/PageNotFound")
-);
-const PaymentDetailsPage = lazy(() =>
-  import("../../pages/PaymentDetailsPage/PaymentDetailsPage")
-);
-const BankInfo = lazy(() => import("../BankInfo/BankInfo"));
-const ReceiptInfo = lazy(() => import("../ReceiptInfo/ReceiptInfo"));
+import Balance from "../Balance/Balance";
+import LangSwitcher from "../LangSwitcher/LangSwitcher";
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/payments" element={<PaymentsPage />} />
-        <Route path="/payments/:paymentId" element={<PaymentDetailsPage />}>
-          <Route path="bank" element={<BankInfo />} />
-          <Route path="receipt" element={<ReceiptInfo />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </Layout>
+    <div>
+      <h1>Redux</h1>
+      <Balance />
+      <LangSwitcher />
+    </div>
   );
 }
 
