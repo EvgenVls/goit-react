a2394fbf311ba6a00abf53e4b7176cc505305037
EvgenVls <145988014+EvgenVls@users.noreply.github.com>
2024-05-10 17:18:40 +0300

    all

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,46 @@
-import { createStore, combineReducers } from "redux";
-import { balanceReducer } from "./balanceSlice";
-import { langReducer } from "./langSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import balanceReducer from "./balanceSlice";
+import langReducer from "./langSlice";
 
-// const initialState = {
-//   balance: {
-//     value: 1000,
-//   },
-//   locale: {
-//     lang: "uk",
-//   },
-// };
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
-const rootReducer = combineReducers({
-  balance: balanceReducer,
-  locale: langReducer,
+const balancePersistConfig = {
+  key: "balanceValue",
+  storage,
+  whitelist: ["value"],
+};
+
+const pBalanceReducer = persistReducer(balancePersistConfig, balanceReducer);
+
+const localePersistConfig = {
+  key: "lang",
+  storage,
+  whitelist: ["lang"],
+};
+
+const pLocaleReducer = persistReducer(localePersistConfig, langReducer);
+
+export const store = configureStore({
+  reducer: {
+    balance: pBalanceReducer,
+    locale: pLocaleReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export const store = createStore(rootReducer);
+export const persistor = persistStore(store);
