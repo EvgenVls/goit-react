a4733eb463df4b18d16afa4e9008da71273bb2cc
Yevgen Vlasenko <evgenvl@ukr.net>
2024-05-13 21:45:00 +0300

    simple redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,7 @@
 import { createStore } from "redux";
 import { devToolsEnhancer } from "@redux-devtools/extension";
-import { statusFilters } from "./costants";
-
-const initialState = {
-  tasks: [
-    { id: 0, text: "Learn HTML and CSS", completed: true },
-    { id: 1, text: "Get good at JavaScript", completed: true },
-    { id: 2, text: "Master React", completed: false },
-    { id: 3, text: "Discover Redux", completed: false },
-    { id: 4, text: "Build amazing apps", completed: false },
-  ],
-  filters: {
-    status: statusFilters.all,
-  },
-};
+import { rootReducer } from "./reducers";
 
 const enhancer = devToolsEnhancer();
-
-const rootReducer = (state = initialState) => {
-  return state;
-};
 
 export const store = createStore(rootReducer, enhancer);
